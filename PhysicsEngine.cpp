e134375ae2888060485ca937b6603e8559bd4986
Wr3tchedTorch <ericmoura511@gmail.com>
2026-03-06 18:58:39 -0300

    finish adding most of the PhysicsEngine definitions

--- PhysicsEngine.cpp
+++ PhysicsEngine.cpp
@@ -9,6 +9,31 @@
 #include <id.h>
 #include <iostream>
 #include <format>
+#include <vector>
+#include <SFML/System/Angle.hpp>
+
+void PhysicsEngine::destroyBody(b2BodyId bodyId)
+{
+	for (int i = 0; i < m_BodyIds.size(); ++i)
+	{
+		auto id = m_BodyIds.at(i);
+		if (B2_ID_EQUALS(id, bodyId))
+		{
+			b2DestroyBody(id); 
+			m_BodyIds.erase(m_BodyIds.begin() + i);
+			return;
+		}
+	}
+}
+
+void PhysicsEngine::cleanList()
+{
+	for (auto id : m_BodyIds)
+	{
+		b2DestroyBody(id);
+	}
+	m_BodyIds.clear();
+}
 
 PhysicsEngine::PhysicsEngine()
 {
@@ -18,7 +43,7 @@ PhysicsEngine::PhysicsEngine()
 	m_WorldId = b2CreateWorld(&worldDef);
 }
 
-void PhysicsEngine::spawnBodyAtLocation(sf::Vector2f location, sf::Vector2f size, sf::Color fillColor)
+void PhysicsEngine::spawnBodyAtLocation(sf::Vector2f location, sf::Vector2f size, sf::Angle rotation, sf::Color fillColor)
 {
 	b2Vec2 locationInMeters =
 	{
@@ -30,8 +55,10 @@ void PhysicsEngine::spawnBodyAtLocation(sf::Vector2f location, sf::Vector2f size
 		converter::pixelsToMeters(size.x),
 		converter::pixelsToMeters(size.y)
 	};
+	b2Rot rotationB2;	
+	rotationB2 = b2MakeRot(rotation.asRadians());
 
-	spawnBodyAtLocation(locationInMeters, sizeInMeters, fillColor);
+	spawnBodyAtLocation(locationInMeters, sizeInMeters, rotationB2, fillColor);
 }
 
 void PhysicsEngine::destroyBodyAtLocation(sf::Vector2f location)
@@ -45,11 +72,12 @@ void PhysicsEngine::destroyBodyAtLocation(sf::Vector2f location)
 	destroyBodyAtLocation(locationInMeters);
 }
 
-void PhysicsEngine::spawnBodyAtLocation(b2Vec2 location, b2Vec2 size, sf::Color fillColor)
+void PhysicsEngine::spawnBodyAtLocation(b2Vec2 location, b2Vec2 size, b2Rot rotation, sf::Color fillColor)
 {
 	b2BodyDef bodyDef = b2DefaultBodyDef();
 	bodyDef.position  = location;
-	bodyDef.type = b2_dynamicBody;
+	bodyDef.rotation  = rotation;
+	bodyDef.type	  = b2_dynamicBody;
 
 	b2BodyId bodyId = b2CreateBody(m_WorldId, &bodyDef);
 
@@ -58,6 +86,10 @@ void PhysicsEngine::spawnBodyAtLocation(b2Vec2 location, b2Vec2 size, sf::Color 
 	b2CreatePolygonShape(bodyId, &shapeDef, &polygon);
 
 	m_BodyIds.push_back(bodyId);
+
+	#ifdef _DEBUG
+	std::cout << std::format("\nPlaced shape at position: (x: {}, y: {}); size: (x: {}, y: {})", location.x, location.y, size.x, size.y);
+	#endif
 }
 
 void PhysicsEngine::destroyBodyAtLocation(b2Vec2 location)
@@ -81,17 +113,26 @@ void PhysicsEngine::destroyBodyAtLocation(b2Vec2 location)
 	if (b2Shape_IsValid(foundShapeId))
 	{
 		#ifdef _DEBUG
-		std::cout << std::format("\nFound shape at position: (x: {}, y: {})", location.x, location.y);
+		std::cout << std::format("\nDestroyed shape at position: (x: {}, y: {})", location.x, location.y);
 		#endif
 
 		b2BodyId bodyId = b2Shape_GetBody(foundShapeId);
-		b2DestroyBody(bodyId);
 
-		removeDestroyedBodyFromList(body);
+		destroyBody(bodyId);
 	}
 }
 
 void PhysicsEngine::update(float delta)
 {
 	b2World_Step(m_WorldId, TIME_STEP, SUB_STEP_COUNT);
+}
+
+b2WorldId PhysicsEngine::getWorld()
+{
+	return m_WorldId;
+}
+
+std::vector<b2BodyId> PhysicsEngine::getBodies()
+{
+	return m_BodyIds;
 }
--- PhysicsEngine.h
+++ PhysicsEngine.h
@@ -4,6 +4,7 @@
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include <math_functions.h>
+#include <SFML/System/Angle.hpp>
 
 struct PointOverlapResultQueryContext
 {
@@ -22,20 +23,16 @@ private:
 
 	std::vector<b2BodyId> m_BodyIds;
 
-	void removeDestroyedBodyFromList(b2BodyId body);
 	void cleanList();
 
-	bool b2OverlapResultFcn(b2ShapeId shapeId, void* context)
-	{
-
-	}
 public:
 	PhysicsEngine();
 
-	void spawnBodyAtLocation(sf::Vector2f location, sf::Vector2f size, sf::Color fillColor);
+	void spawnBodyAtLocation(sf::Vector2f location, sf::Vector2f size, sf::Angle rotation, sf::Color fillColor);
 	void destroyBodyAtLocation(sf::Vector2f location);
+	void destroyBody(b2BodyId body);
 
-	void spawnBodyAtLocation(b2Vec2 location, b2Vec2 size, sf::Color fillColor);
+	void spawnBodyAtLocation(b2Vec2 location, b2Vec2 size, b2Rot rotation, sf::Color fillColor);
 	void destroyBodyAtLocation(b2Vec2 location);
 
 	void update(float delta);
